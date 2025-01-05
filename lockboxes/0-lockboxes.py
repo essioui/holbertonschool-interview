4c3293405dde3bfeec5d63d4190a98d9ae5a4c57
essioui <salahessioui@gmail.com>
2025-01-05 17:07:39 +0100

    Lockboxes

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Lockboxes
+"""
+
+def canUnlockAll(boxes):
+    """
+    Can Unlock All
+    Args:
+        boxes is a list of lists
+    Return:
+        True, False
+    """
+    n = len(boxes)
+    # Condition boxed closed
+    opened = [False] * n
+    # first box always open
+    opened[0] = True
+    # start with first key
+    keys = [0]
+
+    while keys:
+        # the instant key
+        curr_keys = keys.pop(0)
+        # read keys in boxes
+        for key in boxes[curr_keys]:
+            # if key of close box
+            if key < n and not opened[key]:
+                # open boxes
+                opened[key] = True
+                # add keys of boxes to list
+                keys.append(key)
+    # verifier if open all boxes
+    return all(opened)
---
+++ lockboxes/files_main/main_0.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+canUnlockAll = __import__('0-lockboxes').canUnlockAll
+
+boxes = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))
