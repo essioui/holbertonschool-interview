69d0c1e1f732bd7c00aeb69250183d92968b4256
essioui <salahessioui@gmail.com>
2025-05-09 22:55:05 +0100

    advanced binary search

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -1,68 +1,46 @@
-#include <stdio.h>
 #include "search_algos.h"
+#include <stdio.h>
 
 /**
- * print_array - Prints the array between indices left and right inclusive
- * @array: pointer to array
- * @left: starting index
- * @right: ending index
- */
-void print_array(int *array, int left, int right)
-{
-	printf("Searching in array: ");
-	for (int i = left; i <= right; i++)
-	{
-		printf("%d", array[i]);
-		if (i < right)
-			printf(", ");
-	}
-	printf("\n");
-}
-
-/**
- * recursive_search - recursively searches for the first occurrence of value
- * @array: pointer to the array
- * @left: left index
- * @right: right index
- * @value: value to search for
+ * advanced_binary - uses advanced binary search to search for first time
+ * a given value appears in a sorted array using recursion
  *
- * Return: index of first occurrence or -1 if not found
- */
-int recursive_search(int *array, int left, int right, int value)
-{
-	int mid;
-
-	if (left > right)
-		return (-1);
-
-	print_array(array, left, right);
-
-	mid = left + (right - left) / 2;
-
-	if (array[mid] == value)
-	{
-		if (mid == left || array[mid - 1] != value)
-			return (mid);
-		return (recursive_search(array, left, mid, value));
-	}
-	else if (array[mid] < value)
-		return (recursive_search(array, mid + 1, right, value));
-	else
-		return (recursive_search(array, left, mid - 1, value));
-}
-
-/**
- * advanced_binary - wrapper to start recursive search
- * @array: pointer to the array
- * @size: size of the array
- * @value: value to search for
+ * @array: pointer to the first element of the sorted array to search in;
+ * array is sorted in ascending order
+ * @size: number of elements in array
+ * @value: the given value to search for
  *
- * Return: index of first occurrence or -1 if not found
+ * Return: the first index where value is located, or -1 on failure
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	if (!array || size == 0)
+	int half = ((size - 1) / 2), result = 0;
+	size_t i = 0;
+
+	if (array == NULL || size < 1)
 		return (-1);
 
-	return (recursive_search(array, 0, (int)size - 1, value));
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++)
+	{
+		printf("%d", array[i]);
+		if (i != (size - 1))
+			printf(", ");
+		else
+			printf("\n");
+	}
+
+	if (array[half] == value && half == 0)
+		return (half);
+	else if (size == 1)
+		return (-1);
+
+	if (array[half] >= value)
+		return (advanced_binary(array, half + 1, value));
+
+	half++;
+	result = advanced_binary(&array[half], size - half, value);
+	if (result == -1)
+		return (-1);
+	return (half + result);
 }
